8ee53b687c6d60dc62bf7cdd40b06af67ac1d4c6
Anthony G. Harris <hanthony1191@gmail.com>
2015-09-22 17:01:59 -0400

    Fixed Tabbing

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,19 +1,19 @@
 def caesar_cipher(text, shift)
-	alphabet = ('a'..'z').to_a
-	newText = ""
-	text.split("").each do |char|
-		unless alphabet.include?(char.downcase)
-			newText << char
+alphabet = ('a'..'z').to_a
+newText = ""
+text.split("").each do |char|
+	unless alphabet.include?(char.downcase)
+		newText << char
+	else
+		newChar = alphabet[(alphabet.index(char.downcase) + shift) % alphabet.size]
+		if char == char.upcase
+			newText << newChar.upcase
 		else
-			newChar = alphabet[(alphabet.index(char.downcase) + shift) % alphabet.size]
-			if char == char.upcase
-				newText << newChar.upcase
-			else
-				newText << newChar
-			end
+			newText << newChar
 		end
 	end
-	return newText.strip
+end
+return newText.strip
 end
 
 print caesar_cipher("  This isn't a @#$%^&-*<>?/[} DRILL!  ", 5)
