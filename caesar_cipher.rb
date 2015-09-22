dae1bf913b52dbe4bfea2f3fbc4b328c7eab5ee6
Anthony G. Harris <hanthony1191@gmail.com>
2015-09-22 17:14:00 -0400

    Added Comments

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,12 +1,17 @@
 def caesar_cipher(text, shift)
 	alphabet = ('a'..'z').to_a
+	#Created an alphabet range then converted to an array
 	newText = ""
 	text.split("").each do |char|
+		#Broke up text input into individual characters for iteration
 		unless alphabet.include?(char.downcase)
+		#To prevent errors on and keep special characters 
 			newText << char
 		else
 			newChar = alphabet[(alphabet.index(char.downcase) + shift) % alphabet.size]
+			#Shift char using array index and wrap z-a using modulus 
 			if char == char.upcase
+			#To maintain capitalization 
 				newText << newChar.upcase
 			else
 				newText << newChar
@@ -14,6 +19,7 @@ def caesar_cipher(text, shift)
 		end
 	end
 	return newText.strip
+	#To eliminate leading and ending spacing
 end
 
 print caesar_cipher("  This isn't a @#$%^&-*<>?/[} DRILL!  ", 5)
