3cab4091c1a3f457c0082e8f911324142e452d56
Anthony G. Harris <hanthony1191@gmail.com>
2015-09-29 22:20:59 -0400

    Added Final Comments

--- substrings.rb
+++ substrings.rb
@@ -1,10 +1,14 @@
 def substrings(string, dict)
     answer = Hash.new(0)
     sections = string.downcase.split(/\W/)
+	#Used Regex to split input into individual words for analysis
     dict.each do |word|
         sections.each do |section|
+		#Double loop checking each dictionary entry against each string word
             if section =~ (/#{word}/)
+			#Regex checks if any part of the dict word is in the string word
                 answer[word] += 1
+				#Then adds the instance to the answer hash 
             end
         end
     end
